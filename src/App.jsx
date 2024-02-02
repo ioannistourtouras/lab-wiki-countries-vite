e1d55587d52a01d3dec0dd0619642ac57527254f
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-02-02 12:29:36 +0200

    Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import Navbar from './components/Navbar'
+import CountryDetailsPage from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React WikiCountries</h1>
+      <div>
+        <Navbar></Navbar>
+        <HomePage></HomePage>
+        <CountryDetailsPage></CountryDetailsPage>
+      </div>
     </div>
   );
 }
